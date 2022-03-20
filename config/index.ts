3c81ae6d828e70def52bb6d04c84415d845fd94f
DaniRomDev <daniromdev@protonmail.com>
2022-03-20 15:54:08 +0000

    Fixed typo and added new resource on laravel category

--- config/index.ts
+++ config/index.ts
@@ -96,7 +96,8 @@ const config: Configuration = {
         laravel: [{ href: "https://laravel-news.com", title: "Laravel news " },
         { href: "https://laravel-code.tips/", title: "Laravel code tips" },
         { href: "https://spatie.be/open-source?search=&sort=-downloads", title: "Spatie open source packages and courses" },
-        { href: "https://laravelexamples.com/", title: "Laravel real code examples" }
+        { href: "https://laravelexamples.com/", title: "Laravel real code examples" },
+        { href: "https://bestlaravel.com/", title: "Best of laravel" }
 
         ],
         devOps: [{
