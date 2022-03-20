ff058fe32e4b68549c8b77a445f3d8da31fc7ec6
DaniRomDev <daniromdev@protonmail.com>
2022-03-20 17:10:28 +0000

    Initial responsive behavior for nav menu

--- components/Menus/MagazineNavMenu.tsx
+++ components/Menus/MagazineNavMenu.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 import classNames from 'classnames'
-import { NavigationRoute } from 'config'
+import config, { NavigationRoute } from 'config'
 import { useTranslation } from 'hooks/useTranslation'
 import { useRouter } from 'next/router'
+import logo from 'public/images/logo/logomark.min.svg'
+import Image from 'next/image'
+import { GithubIcon, LinkedinIcon } from 'components/Shared/Icons'
+import MobileMenu from './MobileMenu'
+import SelectLocale from './SelectLocale'
 
 const MagazineNavItem: React.FC<NavigationRoute> = ({ href, text }) => {
   const router = useRouter()
@@ -18,7 +23,7 @@ const MagazineNavItem: React.FC<NavigationRoute> = ({ href, text }) => {
           'px-3 py-2 font-medium text-sm text-white rounded-md hover:bg-gray-700 hover:text-white'
         )}
       >
-        {text}
+        {translate(text)}
       </a>
     </Link>
   )
@@ -27,16 +32,33 @@ const MagazineNavItem: React.FC<NavigationRoute> = ({ href, text }) => {
 const MagazineNavMenu: React.FC<{ items: NavigationRoute[] }> = ({
   items = []
 }) => {
+  const renderIcon = (name: string) => {
+    switch (name) {
+      case 'github':
+        return <GithubIcon />
+        break
+      case 'linkedin':
+        return <LinkedinIcon />
+        break
+      default:
+        return null
+    }
+  }
+
   return (
-    <nav className="pt-5 font-sans bg-gray-900">
+    <nav className="pt-5 font-serif bg-gray-900">
       <div className="mx-auto xs:px-4 sm:px-10">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex items-center justify-center flex-1 xs:justify-start sm:justify-start">
             <div className="flex items-center flex-shrink-0">
-              <Link href="/">Logo</Link>
+              <Link href="/" passHref>
+                <a>
+                  <Image src={logo} alt="logo" />
+                </a>
+              </Link>
             </div>
 
-            <div className="hidden sm:hidden lg:block lg:ml-12">
+            <div className="hidden sm:block sm:ml-8 md:ml-6 lg:ml-12">
               <div className="flex space-x-4 tracking-wide">
                 {items.map((item) => (
                   <MagazineNavItem {...item} key={item.href} />
@@ -44,6 +66,50 @@ const MagazineNavMenu: React.FC<{ items: NavigationRoute[] }> = ({
               </div>
             </div>
           </div>
+
+          <div className="absolute inset-y-0 right-0 hidden sm:flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="relative ml-3">
+              <div className="lg:inline">
+                {Object.keys(config.accounts).map((account) => (
+                  <a
+                    className="items-center inline-block mr-3 text-2xl text-gray-200 no-underline align-middle hover:text-white"
+                    href={config.accounts[account].href}
+                    key={account}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {renderIcon(account)}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden sm:block">
+            <SelectLocale />
+          </div>
+          <button
+            type="button"
+            className="items-center justify-center p-2 text-gray-400 bg-gray-700 rounded-md inline-flex sm:hidden hover:text-white hover:bg-gray-700 focus:outline-none"
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+          >
+            <svg
+              className="block w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
 
