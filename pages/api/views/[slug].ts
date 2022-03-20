88f39d90a1a5f73237482eae9a879e94943707d0
DaniRomDev <daniromdev@protonmail.com>
2022-03-20 10:28:53 +0000

    New article php tips

--- pages/api/views/[slug].ts
+++ pages/api/views/[slug].ts
@@ -14,7 +14,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 
         res.setHeader(
             'Cache-Control',
-            'public, s-maxage=1200, stale-while-revalidate=600'
+            'public, s-maxage=600, stale-while-revalidate=300'
         );
 
         return res.status(200).json({
